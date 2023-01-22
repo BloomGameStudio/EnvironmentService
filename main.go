433bfa516a9495d0def8ffc15671fc922a7c2614
Balu <baluproton+gitlab@protonmail.com>
2023-01-22 14:41:35 +0100

    Preemptive Viper Config Settings

--- main.go
+++ main.go
@@ -7,10 +7,20 @@ import (
 
 func main() {
 
+	// viper.SetConfigName("config")
+	// viper.AddConfigPath("config/")
+
+	// err := viper.ReadInConfig()
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	e := echo.New()
 
+	// Enable Echo Debug mode if we are In DEBUG mode. Debug mode sets the log level to DEBUG.
+	// e.Debug = viper.GetBool("DEBUG")
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+
 }
