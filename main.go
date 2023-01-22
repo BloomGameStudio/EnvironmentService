3a6115f8c653e5d9c52f6aad95f19c1df17bc6db
Balu <baluproton+gitlab@protonmail.com>
2023-01-22 14:43:58 +0100

    Viper Activation

--- main.go
+++ main.go
@@ -3,22 +3,23 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/spf13/viper"
 )
 
 func main() {
 
-	// viper.SetConfigName("config")
-	// viper.AddConfigPath("config/")
+	viper.SetConfigName("config")
+	viper.AddConfigPath("config/")
 
-	// err := viper.ReadInConfig()
-	// if err != nil {
-	// 	panic(err)
-	// }
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
 
 	e := echo.New()
 
 	// Enable Echo Debug mode if we are In DEBUG mode. Debug mode sets the log level to DEBUG.
-	// e.Debug = viper.GetBool("DEBUG")
+	e.Debug = viper.GetBool("DEBUG")
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
