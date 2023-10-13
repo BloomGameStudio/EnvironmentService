02f8365ddf2df04595d8be091ee2c19c9cbd45a6
Balu <baluproton+gitlab@protonmail.com>
2023-10-13 13:20:04 +0200

    Validation method for Scripts

--- models/private/scripts.go
+++ models/private/scripts.go
@@ -1,4 +1,13 @@
-package scripts
+package models
 
 type Scripts struct {
+}
+
+func (s Scripts) IsValid() bool {
+
+	// Validates the State
+	// Additional validation and hooks for the State validation can be added here
+	// WARNING: Validation should be scoped to the State
+
+	return true
 }
